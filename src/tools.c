ff97de5625eca399fc20ce15f7b22ccce319cde9
Guillaume W. Bres <guillaume.bressaix@gmail.com>
2020-06-12 09:48:57 +0200

    moved to tools

--- src/tools.c
+++ src/tools.c
@@ -1,5 +1,6 @@
-#include "tools.h"
+#include <stdlib.h>
 #include <stdio.h>
+#include "tools.h"
 
 void print_array (float *array, unsigned int size)
 {
@@ -9,6 +10,19 @@ void print_array (float *array, unsigned int size)
 		printf("%f, ", array[i]);
 	} 
 	printf("%f\n", array[size-1]);
+}
+
+void print_clusters (float **array, unsigned int n_clusters, unsigned int size)
+{
+	unsigned int i, j;
+	for (i=0; i<n_clusters; i++)
+	{
+		for (j=0; j<size-1; j++)
+		{
+			printf("%f, ", array[i][j]);
+		} 
+		printf("%f\n", array[i][size-1]);
+	}
 }
 
 int array2csv (const char *fp, float *array, unsigned int size)
@@ -30,4 +44,13 @@ int array2csv (const char *fp, float *array, unsigned int size)
 
 	fclose(fd);
 	return 0;
+}
+
+void randnf (float *x, unsigned int size)
+{
+	unsigned int i;
+	for (i=0; i<size; i++)
+	{
+		x[i] = (float)rand()/RAND_MAX; 
+	}
 }
--- src/tools.h
+++ src/tools.h
@@ -1,7 +1,10 @@
 #ifndef __TOOLS_H__
 #define __TOOLS_H__
 
+void randnf (float *x, unsigned int size);
+
 void print_array (float *array, unsigned int size);
+void print_clusters (float **array, unsigned int n_clusters, unsigned int size);
 int array2csv (const char *fp, float *array, unsigned int size);
 
 #endif
