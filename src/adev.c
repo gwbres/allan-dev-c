b576e00403bf688e94fee76bf544664b947a4904
Guillaume W. Bres <guillaume.bressaix@gmail.com>
2020-06-12 09:48:30 +0200

    working on tau axis tools, working on is_fractionnal option

--- src/adev.c
+++ src/adev.c
@@ -1,6 +1,40 @@
 #include <math.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include "adev.h"
+
+void tau_axis_pow2 (int *x, unsigned int size)
+{
+	unsigned int i;
+	for (i=0; i<size; i++)
+	{
+		x[i] = (int)roundf(powf(2,i));
+	}
+}
+
+void tau_axis_pow10 (int *x, unsigned int size)
+{
+	unsigned int i;
+	for (i=0; i<size; i++)
+	{
+		x[i] = (int)roundf(powf(10,i));
+	}
+}
+
+void tau_axis (int *x, unsigned size, uint8_t is_pow)
+{
+	unsigned int i;
+	if (is_pow & POWER2_TAU_AXIS)
+		tau_axis_pow2(x, size);
+
+	else if (is_pow & POWER10_TAU_AXIS)
+		tau_axis_pow10(x, size);
+	
+	else {
+		for (i=0; i<size; i++)
+			x[i] = i;
+	}
+}
 
 void split (
 	float *x, float **y, unsigned int n_cluster, unsigned int size
@@ -21,17 +55,10 @@ float mean (float *x, unsigned int size)
 	unsigned int i;
 	float acc = 0.f;
 	for (i=0; i<size; i++)
-		acc += x[i];
-	return acc / (float)size;
-}
-
-void randnf (float *x, unsigned int size)
-{
-	unsigned int i;
-	for (i=0; i<size; i++)
 	{
-		x[i] = (float)rand()/RAND_MAX; 
+		acc += x[i];
 	}
+	return acc / (float)size;
 }
 
 float stdvar (float *x, unsigned int size)
@@ -51,30 +78,73 @@ float stddev (float *x, unsigned int size)
 	return sqrtf(stdvar(x,size));
 }
 
-float avar (float **x, unsigned int size, unsigned int clust_size)
-{
-	// to get the variance v(tau) for a given tau
-	//  compute mean of cluster & adjacent cluster
-	//  compute difference^2
-	//  avarage for all cluster of idnetical size
-	unsigned int i;
-	unsigned int n_clusters = size/clust_size;
-	float acc, xn, xnp1;
+float avar_calc (float **x, 
+		unsigned int n_clusters, 
+		unsigned int clust_size,
+		uint8_t is_fractionnal
+){
+	int i, max;
+	float acc;
+	float xn, xnp1, xnp2;
 
-	acc = 0.f;
-	for (i=0; i<n_clusters-1; i++) // n_clusters+1!
-	{
-		xnp1 = mean(x[n_clusters+1], clust_size);
-		xn = mean(x[n_clusters], clust_size);
-		acc += powf(xnp1 - xn,2);
-	}
-	acc = acc / (float)n_clusters;
+	if (is_fractionnal)
+		max = n_clusters-2;
+	else
+		max = n_clusters-1;
 	
-	return 0.5*acc;
+	acc = 0.f;
+	for (i=0; i<max; i++) 
+	{
+		if (is_fractionnal)
+		xnp2 = mean(x[i+2], clust_size);
+		xnp1 = mean(x[i+1], clust_size);
+		xn   = mean(x[i],	  clust_size);
+
+		if (is_fractionnal)
+			acc += powf(xnp2  -2*xnp1 + xn, 2);
+		else
+			acc += powf(xnp1 - xn, 2);
+	}
+
+	if (is_fractionnal)
+		return acc /2.0 /((float)max+1.0) /(float)n_clusters;
+	else
+		return acc /(float)n_clusters /2.0;
 }
 
-float adev (float *data, int size, int *tau, int n_cluster)
+void avar_tau_pow2 (float *x, float *y, unsigned int size, uint8_t is_fractionnal) 
 {
-//	return sqrtf(avar(data,size,tau,n_cluster));
-	return 0.f;
+	unsigned int n_clusters, clusters_size;
+	unsigned int index = (int)log2(size/2)+1; // last tau offset
+
+	float **clusters;
+
+	clusters = (float**)malloc(size*size*sizeof(float)); // max. nb of clusters
+	for (n_clusters=0; n_clusters < size; n_clusters++)
+	{
+		// max. cluster size
+		clusters[n_clusters] = (float*)malloc(size*sizeof(float)); 
+	}
+
+	for (n_clusters=1; n_clusters <= size/2; n_clusters *= 2)
+	{
+		clusters_size = size / n_clusters;
+		split(x, clusters, n_clusters, clusters_size);
+		y[index] = avar_calc(clusters, n_clusters, clusters_size, is_fractionnal);
+		index--;
+	}
+
+	y[0] = stdvar(x, size); // 1st tau offset is always std var
+	
+	for (n_clusters=0; n_clusters < size; n_clusters++)
+		free(clusters[n_clusters]);
+	free(clusters);
+}
+
+void adev_tau_pow2 (float *x, float *y, unsigned int size, uint8_t is_fractionnal)
+{
+	unsigned int i;
+	avar_tau_pow2(x, y, size, is_fractionnal);
+	for (i=0; i<size; i++)
+		y[i] = sqrtf(y[i]);
 }
--- src/adev.h
+++ src/adev.h
@@ -1,35 +1,81 @@
 #ifndef __ADEV_H__
 #define __ADEV_H__
 
-/*
-	splits *x into **y:
-	n clusters of cluster_size=n_cluster/size size;
-	returns cluster_size
-*/
-void split (float *x, float **y, unsigned int n_clust, unsigned int clust_size);
+#include <stdint.h>
 
-void randnf (float *x, unsigned int size);
+///////////////////////////////
+//       TAUs               //
+/////////////////////////////
 
-/*
-	computes statistic mean of data set
-*/
-float mean (float *x, unsigned int size);
+// tau_axis:
+//  creates Tau axis of requested size
+//  axis = DEFAULT_TAU_AXIS:
+//		+ creates a tau axis ranging from {0:size-1}
 
-/*
-	compute standard variance & deviance
-	of given *x data set
-*/
+//  axis = POWER2_TAU_AXIS:
+//		+ creates a tau axis rangin from {2^0,2^size-1}
+
+//  axis = POWER10_TAU_AXIS:
+//		+ creates a tau axis rangin from {10^0,10^size-1}
+void tau_axis (int *tau, unsigned int size, uint8_t axis);
+
+// tau_axis_pow2:
+//  creates a Tau axis made only of powers of 2
+void tau_axis_pow2 (int *tau, unsigned int size);
+
+// tau_axis:
+//  creates a Tau axis made only of powers of 10
+void tau_axis_pow10 (int *tau, unsigned int size);
+
+//////////////////////////////
+//       VARIANCEs         //
+////////////////////////////
+
+// avar:
+//  returns Allan deviation
+//  float *x: input raw/fractionnal data
+//  unsigned int size: x array allocated size
+//  uint8_t is_fractionnal: set 0x01 if *x is fractionnal data
+void avar_tau_pow2 (float *x, float *y, unsigned int size, uint8_t is_fractionnal); 
+
+// stdvar:
+//  returns standard variance
+//  float *x: input data
+//  unsigned int size: *x allocated size
 float stdvar (float *x, unsigned int size);
+
+//////////////////////////////
+//       DEVIATIONs        //
+////////////////////////////
+
+// adev:
+//  returns Allan deviation
+//  float *x: input raw/fractionnal data
+//  unsigned int size: x array allocated size
+//  uint8_t is_fractionnal: set 0x01 if *x is fractionnal data
+void adev_tau_pow2 (float *x, float *y, unsigned int size, uint8_t is_fractionnal);
+
+// stddev
+//  returns standard deviation 
+//  float *x: input data
+//  unsigned int size: x array allocated size
 float stddev (float *x, unsigned int size);
 
-/*
-	AVAR
-*/
-float avar (float **x, unsigned int size, unsigned int clust_size);
+//////////////////////////////////////////////////////////////////
 
-/*
-	ADEV.
-*/
-float adev (float *x, int size, int *tau, int n_cluster);
+#define  POWER2_TAU_AXIS 0
+#define POWER10_TAU_AXIS 1
+
+// mean:
+//  returns mean of *x data set
+float mean (float *x, unsigned size);
+
+// split:
+//  splits *x array into **y subsets
+void split (float *x, float **y, unsigned int n_clusters, unsigned int clust_size);
+
+// avar_calc
+//  returns AVAR() for input clusters **x 
+float avar_calc (float **x, unsigned int n_clusters, unsigned int clust_size, uint8_t fractionnal);
 
 #endif
